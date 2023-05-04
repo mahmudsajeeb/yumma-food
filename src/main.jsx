0fc4cadeb7285f781487337b3cba9d0dbc90c345
Mahmud sajib <mahmudsajib082@gmail.com>
2023-05-04 21:21:36 +0600

    home page blur

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import {  RouterProvider} from "react-router-dom";
 import router from './routes/Routes.jsx';
 import AuthProvider from './Provider/AuthProvider';
 import dotenv from 'dotenv';
-dotenv.config();
+// dotenv.config();
  
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
